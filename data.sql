8898ef36f5676549c3fb8a5e5b63e2337fe8f5c8
Saranya <saranyas2323@gmail.com>
2026-07-14 21:07:16 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,23 @@
+-- Insert Customers
+INSERT INTO customers VALUES
+(1, 'Saranya', 'Chennai'),
+(2, 'Rahul', 'Bangalore'),
+(3, 'Anu', 'Hyderabad');
+
+-- Insert Products
+INSERT INTO products VALUES
+(1, 'Laptop', 50000),
+(2, 'Mobile', 20000),
+(3, 'Headphones', 2000);
+
+-- Insert Orders
+INSERT INTO orders VALUES
+(1, 1, '2024-01-10'),
+(2, 2, '2024-01-11'),
+(3, 1, '2024-01-12');
+
+-- Insert Order Details
+INSERT INTO order_details VALUES
+(1, 1, 1, 1),
+(2, 2, 2, 2),
+(3, 3, 3, 3);
